6049edb4f6c84e8d99c3cb16bee4d7284bce32ad
h4ckit <dammysteve10@gmail.com>
2022-10-03 19:36:02 +0100

    [add] Added New Routes For CarWash Registration, Credential Caching And Retrieval

--- infra/mux/router/router.go
+++ infra/mux/router/router.go
@@ -1,17 +1,27 @@
 package router
 
 import (
+	"car_wash/infra/auth/authenticator"
 	"car_wash/infra/mux/controller"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func InitRouter(controller *controller.Controller) *mux.Router {
+func InitRouter(controller *controller.Controller, authHandler *authenticator.Authenticator) *mux.Router {
 	router := mux.NewRouter()
-	pathSubrouter := router.PathPrefix("/carwash").Subrouter()
 
-	pathSubrouter.HandleFunc("", controller.UpgradeWss).Methods(http.MethodGet)
-	pathSubrouter.HandleFunc("/registerOwner", controller.RegisterNewOwner).Methods(http.MethodPost)
+	authenticationRoutes := router.PathPrefix("/auth").Subrouter()
+	authenticationRoutes.HandleFunc("/register", controller.RegisterNewOwner).Methods(http.MethodPost)
+	authenticationRoutes.HandleFunc("/cache", controller.AddCredsToJar).Methods(http.MethodPost)
+	authenticationRoutes.Use(authHandler.BearerAuth)
+
+	pathSubrouter := router.PathPrefix("/carwash").Subrouter()
+	pathSubrouter.HandleFunc("/wss", controller.UpgradeWss).Methods(http.MethodGet)
+	pathSubrouter.HandleFunc("/register", controller.RegisterCarWash).Methods(http.MethodPost)
+	pathSubrouter.HandleFunc("/wash", controller.RegisterWash).Methods(http.MethodPost)
+	pathSubrouter.Use(authHandler.APIAuth)
+
+	router.HandleFunc("/auth/check/{hash}", controller.CheckHash).Methods(http.MethodGet)
 
 	return router
 }
