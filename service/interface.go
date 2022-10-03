967a6ae8ef51a10b107eab13b5aaa43e87834cf1
h4ckit <dammysteve10@gmail.com>
2022-10-03 19:40:42 +0100

    [add] Added More Service Layer Method Specifications

--- service/interface.go
+++ service/interface.go
@@ -1,8 +1,16 @@
 package service
 
-import "car_wash/model"
+import (
+	"car_wash/model"
+	"context"
+)
 
 type Svc interface {
-	RegisterNewOwner(owner model.Owner) error
-	FetchDataByDate(date string) (model.WebSocketResult, error)
+	SaveWashDetails(ctx context.Context, wash model.Wash) error
+	RegisterCarWash(ctx context.Context, carWash model.CarWash) (string, error)
+	RegisterNewOwner(ctx context.Context, owner model.Owner) (string, error)
+	FetchDataByDate(ctx context.Context, date string) (model.WebSocketResult, error)
+	GetUpdatesChannel(ctx context.Context) <-chan model.Wash
+	CacheCreds(ctx context.Context, hash string) error
+	CheckCreds(hash string) (string, error)
 }
