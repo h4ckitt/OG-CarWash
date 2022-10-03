ad4972b58a13728c0a45c37cf45e499c745e1081
h4ckit <dammysteve10@gmail.com>
2022-10-03 19:38:33 +0100

    [add] Added New Data Persistence And Retrieval Method Specifications

--- repository/repo_interface.go
+++ repository/repo_interface.go
@@ -3,6 +3,13 @@ package repository
 import "car_wash/model"
 
 type Repo interface {
+	RegisterCarWash(userID string, carWash model.CarWash) (string, error)
 	RegisterOwner(owner model.Owner) error
-	FetchDataByDate(date string) (model.WebSocketResult, error)
+	FetchCarWashDataByDate(date string, carWashID string) (model.WebSocketResult, error)
+	FetchAllCarWashDataByDate(date string, ownerID string) (model.WebSocketResult, error)
+	VerifyAPIKey(key string) (string, error)
+	RetrieveAPIKey(uid string) (string, error)
+	StoreAPIKey(uid string, key string) error
+	UpdateAPIKey(uid string, key string) error
+	SaveWashDetails(uid string, wash model.Wash) error
 }
