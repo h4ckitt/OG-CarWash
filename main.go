96d3be2f3d346294ffc78690f86c56a8c1c906ef
h4ckitt <dammysteve10@gmail.com>
2023-01-29 04:09:10 +0100

    [refactor] Moved Port Specification From Positional Argument To Env

--- main.go
+++ main.go
@@ -10,20 +10,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"regexp"
 )
 
 func main() {
-
-	if len(os.Args) < 2 {
-		fmt.Printf("Usage: %s <port>\n", os.Args[0])
-		return
-	}
-
-	if match, err := regexp.MatchString("^[0-9]+$", os.Args[1]); err != nil || !match {
-		log.Fatalln("invalid port specified")
-	}
 
 	if err := config.Load(); err != nil {
 		log.Fatalln(err)
@@ -48,7 +37,7 @@ func main() {
 
 	log.Println("Starting Server ......")
 
-	if err := http.ListenAndServe(fmt.Sprintf(":%s", os.Args[1]), r); err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", config.GetConfig().RunConfig.Port), r); err != nil {
 		log.Panicln(err)
 	}
 
